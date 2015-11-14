10833284c4cfd6755f9ce493ce99b54e07c51c81
Danny Cypher <Danny Cypher>
2015-11-14 14:50:47 -0500

    Initial commit

---
+++ workspace/Calander-refactoring/src/src/Driver.java
@@ -0,0 +1,23 @@
+package src;
+import java.io.IOException;
+
+import src.com.std.controller.CalendarController;
+import src.com.std.model.CalendarModel;
+import src.com.std.view.CalendarView;
+
+/**
+ * Load the GUI to run the Calendar application
+ * 
+ * @author xxx
+ *
+ */
+public class Driver {
+	public static void main(String[] args) throws IOException {
+		
+		CalendarModel model = new CalendarModel();
+		CalendarView view = new CalendarView();
+		
+		CalendarController controler = new CalendarController(model, view);
+		view.setVisible(true);	
+	}
+}
---
+++ workspace/Calander-refactoring/src/src/com/std/controller/listener/NewAppointmentActionListener.java
@@ -0,0 +1,41 @@
+package src.com.std.controller.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import src.com.std.controller.CalendarController;
+import src.com.std.controller.dialog.AppointmentDialog;
+import src.com.std.model.CalendarModelUtility;
+import src.com.std.model.appointment.RefAppointment;
+
+public class NewAppointmentActionListener implements ActionListener {
+
+	/**
+	 * a reference to the controller so that this listener
+	 * can access both the model and the view.
+	 */
+	private CalendarController controller;
+
+	/**
+	 * creates a new AppointmentSelectionMouseListener
+	 * 
+	 * @param CalendarControler cc is the reference to the controller 
+	 */
+	public NewAppointmentActionListener(CalendarController cc){
+		controller = cc;
+	}
+		
+	/**
+	 * This whent the user clicks the new appointent button, it creats
+	 * a dialogue box that allows the user to enter new information
+	 */
+	
+	public void actionPerformed(ActionEvent e) {
+		RefAppointment ref = CalendarModelUtility.getNewAppointment(controller.getModel());
+		
+		if(AppointmentDialog.changeAppointment(controller.getView(), ref)) {
+			CalendarModelUtility.addUsingPattern(controller.getModel(), ref);
+		}
+	}
+
+}
